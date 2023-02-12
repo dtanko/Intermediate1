e1e415e2df9c0c538674554ef63aea5d1f4c28b2
dtanko <116301147+dtanko@users.noreply.github.com>
2023-02-12 23:29:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function Multiply (x, y) {
 
     // call Multiply
 var mul = Multiply (2, 3);
-    // output 6
+console.log(mul)
 
 let f_name= "David", l_name = "Tanko", stutern_track = "Front-end Developer", skills = "HTML, CSS and JavaScript", year = 1990, colour_1="white";
 var f_snt="Hi!, my names is " + f_name +" " +l_name + "and I am currently enrolled at " + stutern_track + "and have acquired these skills: " + skills+". My country Nigeria had her independence in " + year + " and hey my favourite colour is "+ colour_1+". Thank you.";
